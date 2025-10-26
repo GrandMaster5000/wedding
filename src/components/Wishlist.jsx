8b979e8029f224e45ed6a50bc847fc96edf2e24f
GrandMaster5000 <ggamer556@mail.ru>
2025-10-26 16:47:08 +0300

    feat: update wishes

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -4,8 +4,11 @@ import './Wishlist.css';
 const Wishlist = () => {
   return (
     <section className="wishlist">
-      <div className="wishlist-decoration">
+      <div className="wishlist-decoration wishlist-decoration-left">
         <img src="/images/decoration-2.svg" alt="Decoration" />
+      </div>
+      <div className="wishlist-decoration wishlist-decoration-bow">
+        <img src="/images/weddin-image.svg" alt="Decoration" />
       </div>
       <div className="container">
         <h2>вишлист</h2>
@@ -14,7 +17,7 @@ const Wishlist = () => {
             Приносите веселье в душе, радость в сердце, а подарки – в конверте!
           </p>
           <p>
-            Будем благодарны, если вместо цветов вы решите подарить нам бутылочку хорошего вина.
+            Будем благодарны, если вместо цветов вы решите подарить нам бутылочку хорошего коньяка.
           </p>
           <p>
             Очень надеемся, что у вас будет возможность оставить детей на этот вечер под присмотром 
